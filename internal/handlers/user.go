1c9cc29af28ae24271dc825b40347cfe70ffdd66
Damien Buttler <damien@doublehops.com>
2023-03-27 08:20:59 +1100

    Extracting validation errors from Gin into preferred format.

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/doublehops/dh-api/internal/responses"
-
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"reflect"
 )
 
 type User struct {
@@ -46,13 +48,47 @@ func ListUser(c *gin.Context) {
 	responses.MultiItemResponse(c, users, p)
 }
 
+type UpdateUserObj struct {
+	Username string `json:"username" binding:"required,min=3"`
+	Country  string `json:"country" binding:"required,min=3,max=3"`
+}
+
+func Validate[T any](c *gin.Context, obj *T) []responses.ValidationField {
+	var errors []responses.ValidationField
+
+	var u UpdateUserObj
+	if bindErrors := c.ShouldBindJSON(obj); bindErrors != nil {
+		for _, v := range bindErrors.(validator.ValidationErrors) {
+			thisError := responses.ValidationField{}
+			field, _ := reflect.TypeOf(&u).Elem().FieldByName(v.Field())
+			fieldName, _ := field.Tag.Lookup("json")
+			thisError[fieldName] = []string{v.Error()}
+
+			errors = append(errors, thisError)
+		}
+		fmt.Printf(">> bindErrors: %+v <<\n", bindErrors)
+	}
+
+	return errors
+}
+
 // UpdateUser - Validation error example.
 func UpdateUser(c *gin.Context) {
-	errors := []responses.ValidationField{
-		{
-			"emailAddress": []string{"email address not valid"},
-		},
-	}
+	user := UpdateUserObj{}
+	//if err := c.ShouldBindJSON(&user); err != nil {
+	//	c.AbortWithStatusJSON(http.StatusBadRequest,
+	//		gin.H{
+	//			"error":   "VALIDATEERR-1",
+	//			"message": "Invalid inputs. Please check your inputs"})
+	//	return
+	//}
+
+	errors := Validate(c, &user)
+	//errors := []responses.ValidationField{
+	//	{
+	//		"emailAddress": []string{"email address not valid"},
+	//	},
+	//}
 
 	responses.ValidationErrorResponse(c, 422, errors)
 }
