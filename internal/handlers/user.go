84ead78e55fa74a6447503fe8b53a63c87f3a43f
Damien Buttler <damien@doublehops.com>
2023-03-27 13:51:12 +1100

    Have validation working.

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,11 +1,13 @@
 package handlers
 
 import (
+	"errors"
 	"fmt"
 	"github.com/doublehops/dh-api/internal/responses"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"reflect"
+	"strings"
 )
 
 type User struct {
@@ -50,19 +52,82 @@ func ListUser(c *gin.Context) {
 
 type UpdateUserObj struct {
 	Username string `json:"username" binding:"required,min=3"`
-	Country  string `json:"country" binding:"required,min=3,max=3"`
+	Country  string `json:"country" binding:"min=3,max=3"`
+}
+
+func GetErrorMessages() map[string]map[string]string {
+	return map[string]map[string]string{
+		"--username": {"required": "This field is required"},
+		"country":    {"required": "This field is required", "min": "Must be at least 3 characters long"},
+	}
+}
+
+func getStandardErrors(tag string) string {
+	errorMap := map[string]string{
+		"required": "this is a required field",
+	}
+
+	if _, ok := errorMap[tag]; ok {
+		return errorMap[tag]
+	}
+
+	return ""
+}
+
+func getErrorMessage(field, tag, ginError string) string {
+	messages := GetErrorMessages()
+	if _, ok := messages[field]; ok {
+		if _, ok = messages[field][tag]; !ok {
+			return messages[field][tag]
+		}
+	}
+
+	stdError := getStandardErrors(tag)
+	if stdError != "" {
+		return stdError
+	}
+
+	return parseGinError(ginError)
+}
+
+func parseGinError(str string) string {
+	pos := strings.Index(str, "Error:")
+	substr := str[pos+6:]
+
+	return substr
 }
 
 func Validate[T any](c *gin.Context, obj *T) []responses.ValidationField {
+	var errs []responses.ValidationField
+
+	err := c.BindQuery(&obj)
+	if err != nil {
+		var ve validator.ValidationErrors
+		if errors.As(err, &ve) {
+			for _, fe := range ve {
+				thisError := responses.ValidationField{}
+				field, _ := reflect.TypeOf(obj).Elem().FieldByName(fe.Field())
+				fieldName, _ := field.Tag.Lookup("json")
+				thisError[fieldName] = []string{getErrorMessage(fieldName, fe.Tag(), fe.Error())}
+				errs = append(errs, thisError)
+			}
+		}
+	}
+
+	return errs
+}
+
+func ValidateSSS[T any](c *gin.Context, obj *T) []responses.ValidationField {
 	var errors []responses.ValidationField
 
-	var u UpdateUserObj
 	if bindErrors := c.ShouldBindJSON(obj); bindErrors != nil {
 		for _, v := range bindErrors.(validator.ValidationErrors) {
 			thisError := responses.ValidationField{}
-			field, _ := reflect.TypeOf(&u).Elem().FieldByName(v.Field())
+			field, _ := reflect.TypeOf(obj).Elem().FieldByName(v.Field())
 			fieldName, _ := field.Tag.Lookup("json")
+			//thisErr := v.(fieldError)
 			thisError[fieldName] = []string{v.Error()}
+			//thisError[fieldName] = []string{getErrorMessage(fieldName, thisError.Tag())}
 
 			errors = append(errors, thisError)
 		}
@@ -74,7 +139,14 @@ func Validate[T any](c *gin.Context, obj *T) []responses.ValidationField {
 
 // UpdateUser - Validation error example.
 func UpdateUser(c *gin.Context) {
-	user := UpdateUserObj{}
+	var user UpdateUserObj
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		fmt.Printf(">>>>>> error: %s\n", err)
+	}
+	fmt.Printf(">>>> user: %s\n", user)
+	//_ = json.Unmarshal(c.Request.Body, &user)
 	//if err := c.ShouldBindJSON(&user); err != nil {
 	//	c.AbortWithStatusJSON(http.StatusBadRequest,
 	//		gin.H{
@@ -82,6 +154,12 @@ func UpdateUser(c *gin.Context) {
 	//			"message": "Invalid inputs. Please check your inputs"})
 	//	return
 	//}
+
+	rawdata, err := c.GetRawData()
+	if err != nil {
+		fmt.Printf(">>>> error: %s\n", err)
+	}
+	fmt.Printf(">>>> raw data: %s\n", rawdata)
 
 	errors := Validate(c, &user)
 	//errors := []responses.ValidationField{
